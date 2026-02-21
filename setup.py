f7e111e0a80400f514b503991f90241caed17f87
dwr-psandhu <psandhu@water.ca.gov>
2026-02-21 08:57:53 -0800

    moved to pyproject toml and scm versioning

--- setup.py
+++ setup.py
@@ -1,30 +1,12 @@
+"""
+Setup script for building the package.
+
+This file is kept for backwards compatibility.
+The package is configured using pyproject.toml.
+"""
+
 from setuptools import setup
-import versioneer
 
-requirements = [
-    # package requirements go here
-]
+if __name__ == "__main__":
+    setup()
 
-setup(
-    name='schismviz',
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
-    description="holoviews visualizations for schism",
-    license="MIT",
-    author="Nicky Sandhu",
-    author_email='psandhu@water.ca.gov',
-    url='https://github.com/cadwrdeltamodeling/schismviz',
-    packages=['schismviz'],
-    entry_points={
-        'console_scripts': [
-            'schismviz=schismviz.cli:cli'
-        ]
-    },
-    install_requires=requirements,
-    keywords='schismviz',
-    classifiers=[
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-    ]
-)
